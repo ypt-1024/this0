9d8169e504f2b5fdc4ab2b5e05bdf4222fdbaaf2
this0 <ypt@this0.com>
2025-01-12 12:23:49 +0800

    feat: 完成引入markdown编辑器，添加顶部菜单

--- src/common/router.js
+++ src/common/router.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
-let routes = [{ path: '/', component: () => import('../views/HomePage.vue') },
-
+let routes = [
+    { path: '/', component: () => import('../views/HomePage.vue') },
+    { path: '/category-list', component: () => import('../views/CategoryList.vue') },
 ]
 
 const router = createRouter({
---
+++ src/views/CategoryList.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>路由示例</div>
+</template>
+
+<style scoped lang="scss"></style>
