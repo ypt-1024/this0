ba5e4ed17e8b648c8fb1ee3c2f0e349f5aca2efe
this0 <ypt@this0.com>
2025-02-07 01:11:33 +0800

    feat: 完成添加后台文章组件，文章发布页简单静态页面

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,37 +1,37 @@
 <template>
 
+
+
   <!--  总面板-->
   <div class="main-panel">
 
-    <!--    菜单面板-->
-    <div class="menus">
+    <n-flex justify="space-between">
 
-      <!--     后台banner-->
-      <div>
-        <n-image
-            width="100"
-            src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
-        />
+      <!--    菜单面板-->
+      <div class="menus">
+
+        <!--     后台banner-->
+        <n-image width="100" src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg" />
+
+        <!--      侧边栏菜单-->
+        <div v-for="(menu, index) in menus" @click="toPage(menu)">
+          {{ menu.name }}
+        </div>
       </div>
 
-      <!--      侧边栏菜单-->
-      <div v-for="(menu, index) in menus" @click="toPage(menu)">
-        {{ menu.name }}
+      <!--    后台子页面路由-->
+      <div style=" width: 85%">
+        <router-view></router-view>
       </div>
-    </div>
-
-    <!--    后台子页面路由-->
-    <div style="padding: 20px; width: 80%">
-      <router-view></router-view>
-    </div>
+    </n-flex>
   </div>
 
 
 </template>
 
 <script setup>
-import {ref, reactive, inject, onMounted, computed} from 'vue'
-import {useRouter, useRoute} from 'vue-router'
+import { ref, reactive, inject, onMounted, computed } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 import cookie from 'js-cookie'
 
 const router = useRouter()
@@ -39,10 +39,10 @@ const route = useRoute()
 
 // 分类列表
 const adminMenus = [
-  {name: '文章管理', href: '/dashboard/article'},
-  {name: '分类管理', href: '/dashboard/category'},
-  {name: '用户中心', href: '/dashboard/user'},
-  {name: '退出', href: 'logout'},
+  { name: '文章管理', href: '/dashboard/article' },
+  { name: '分类管理', href: '/dashboard/category' },
+  { name: '用户中心', href: '/dashboard/user' },
+  { name: '退出', href: 'logout' },
 ]
 const menus = ref(adminMenus)
 
@@ -72,22 +72,21 @@ const toPage = (menu) => {
 </script>
 
 <style lang="scss" scoped>
+//主面板
 .main-panel {
+  background-color: #EDECEA;
   font-size: large;
   font-weight: bold;
-  display: flex;
-  color: #64676a;
-  max-width: 100%;
 }
 
+//侧边菜单
 .menus {
+  border-radius: 10px;
   line-height: 55px;
   text-align: center;
-  width: 15%;
-  height: 100%;
-  border-right: 2px solid #dadada;
+  width: 13%;
+  background-color: #F7F7F5;
+  //border-right: 2px solid #dadada;
   cursor: pointer;
 }
-
-
 </style>
